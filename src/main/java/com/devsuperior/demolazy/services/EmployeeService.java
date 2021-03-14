6d859258b35a0bc047e8baa1408062613f544da2
Guilherme <guiihh532@gmail.com>
2021-03-14 10:23:43 -0300

    Project Create service in base generics

---
+++ src/main/java/com/devsuperior/demolazy/services/EmployeeService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.demolazy.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.demolazy.dto.EmployeeDTO;
+import com.devsuperior.demolazy.entities.Employee;
+import com.devsuperior.demolazy.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService implements GenericService<Employee, EmployeeDTO, Long>{
+
+
+	@Autowired
+	private EmployeeRepository repository;
+
+	@Override
+	public JpaRepository<Employee, Long> getRepository() {
+		// TODO Auto-generated method stub
+		return repository;
+	}
+
+	
+	
+}
